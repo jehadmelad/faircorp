830a86a0ef138ac5ce8485ba4727e01a9245633f
jehadmelad <jd.melad@gmail.com>
2020-11-23 22:52:42 +0100

    using Anotation @Autowire @Components @Bean

--- src/main/java/com/emse/spring/faircorp/FaircorpApplicationConfig.java
+++ src/main/java/com/emse/spring/faircorp/FaircorpApplicationConfig.java
@@ -2,6 +2,7 @@ package com.emse.spring.faircorp;
 
 import com.emse.spring.faircorp.hello.ConsoleGreetingService;
 import com.emse.spring.faircorp.hello.GreetingService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -10,18 +11,25 @@ import org.springframework.stereotype.Service;
 
 @Configuration
 public class FaircorpApplicationConfig {
+    @Autowired
+    GreetingService greetingService ;
+
+    @Autowired
+    ConsoleGreetingService greeting;
 
     @Bean
-    public CommandLineRunner greetingCommandLine(){
+    public CommandLineRunner greetingCommandLine(GreetingService greetingService){
 
-        GreetingService greetingService ;
+        this.greetingService = greetingService;
         return new CommandLineRunner( ) {
 
             @Override
             public void run(String... args) throws Exception {
+                greeting.greet("jehad");
+//                ConsoleGreetingService greeting = new ConsoleGreetingService();
 
-                ConsoleGreetingService greeting = new ConsoleGreetingService();
-                greeting.greet("Jehad");
+
+//                greeting.greet("Jehad");
                 // (4)
             }
         };
